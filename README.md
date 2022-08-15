56d489696889c92ea389c9dfd528fd8615e03f68
ext.zhuyunfeng1 <i@zhuyunfeng.com>
2022-08-15 16:31:16 +0800

    fix: support vite

--- README.md
+++ README.md
@@ -1,24 +1,4 @@
 # 数据大屏
 
-## Project setup
-```
-npm install
-```
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+修改 buffer 支持 Unit8Array
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+// import polyfillNode from "rollup-plugin-polyfill-node";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 8080
-  }
-})
+    port: 8080,
+  },
+  resolve: {
+    alias: {
+      'crypto': 'crypto-browserify',
+      'stream': 'stream-browserify'
+    }
+  },
+  optimizeDeps: {
+    force: true,
+    // exclude: ["crypto"], // <= The libraries that need shimming should be excluded from dependency optimization.
+  },
+  define: {
+    'process.env.NODE_DEBUG': 'false'
+  },
+});
